62839ff9d7cbc270a7ce98253f2b8248fd91cae0
juanssosa <>
2022-10-18 19:29:51 -0500

    Validate Email

---
+++ BDD/validateEmail.test.js
@@ -0,0 +1,9 @@
+const validaEmail = require('./validateEmail');
+
+test('email juansosa@gmail.com -> true', ()=>{
+    expect(validaEmail.isValidEmail('juansosa@gmail.com')).toBe(true) 
+});
+
+test('email sdsd -> false', ()=>{
+    expect(validaEmail.isValidEmail('sdsd')).toBe(false) 
+});
